58dbc43eb392aad88a54613c7f27c30a0e2b49a6
Rain649 <986550410@qq.com>
2021-02-21 17:13:14 +0800

    取消无用文件提交

--- build/preception/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lsj/dev/Mine_WS/src/preception
-# Build directory: /home/lsj/dev/Mine_WS/build/preception
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
